5a8b6592d642dde9d927d3265d08cac622474283
gitbush-test <82594147+megi-j@users.noreply.github.com>
2023-02-04 22:19:07 +0400

    style: create home page with its styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-
+import styled from "styled-components";
+import bg from "./images/bg.png";
+import logo from "./images/LOGO.png";
+import logo2 from "./images/LOGO-2.png";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header>
+        <img src={logo} alt="logo" />
+      </Header>
+      <Main>
+        <Button>ᲠᲔᲖᲘᲣᲛᲔᲡ ᲓᲐᲛᲐᲢᲔᲑᲐ</Button>
+        <Logo src={logo2} />
+      </Main>
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  max-width: 1920px;
+  width: 100%;
+  height: 100vh;
+  margin: 0 auto;
+  background-image: url(${bg});
+`;
+const Header = styled.header`
+  width: 90%;
+  height: 89px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #1a1a1a;
+`;
+const Button = styled.button`
+  width: 464px;
+  height: 60px;
+  background: #1a1a1a;
+  border-radius: 8px;
+  color: #fff;
+  font-weight: 500;
+  font-size: 20px;
+  z-index: 1;
+`;
+const Main = styled.main`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+const Logo = styled.img`
+  z-index: 10;
+  position: absolute;
+  top: 45%;
+  left: 55%;
+`;
